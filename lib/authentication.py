545cfcc481caeba9a30b0230b0bb8bc1bf6d2089
Elena <742042@gmail.com>
2020-05-23 16:25:44 -0700

    add positions

--- lib/authentication.py
+++ lib/authentication.py
@@ -2,20 +2,22 @@ import json
 
 import requests
 
+
 class Authenticate(object):
-    def __init__(self):
+    def __init__ (self):
         self.base_url = 'https://recruit-portnov.herokuapp.com/recruit/api/v1'
         self.session = requests.Session()
-    #every time you create instance of authenticate init will be triggered
-    # need to add self in passing-> def authenticate (self, base_url, email, password)->
-    # to be able to have access to Session -> self.requests.Session(), now instead
-    # request.get -> Session.get => instead create request every time, will have session
-    # and executing all request on top this session
-        self.session.headers.update({'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'})
+        # every time you create instance of authenticate init will be triggered
+        # need to add self in passing-> def authenticate (self, base_url, email, password)->
+        # to be able to have access to Session -> self.requests.Session(), now instead
+        # request.get -> Session.get => instead create request every time, will have session
+        # and executing all request on top this session
+        self.session.headers.update({
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36'})
+
     # created Chrome session
 
-
-    def authenticate(self, email, password):
+    def authenticate (self, email, password):
         resp = self.session.post(self.base_url + '/login', json={"email": email, "password": password})
         json_parsed = json.loads(resp.text)
         token = json_parsed.get('token', None)
@@ -23,29 +25,30 @@ class Authenticate(object):
             self.session.headers.update({'Authorization': 'Bearer ' + token})
         return resp
 
-    def post_new_candidate(self, first_name, last_name, email, password):
+    def post_new_candidate (self, first_name, last_name, email, password):
         json_data = {"firstName": first_name, "lastName": last_name, "email": email, "password": password}
         return self.session.post(self.base_url + '/candidates', json=json_data)
 
-
-    def delete_candidate_by_id(self, new_candidate_id):
+    def delete_candidate_by_id (self, new_candidate_id):
         return self.session.delete(self.base_url + '/candidates/' + str(new_candidate_id))
 
-
-    def get_all_candidates(self):
+    def get_all_candidates (self):
         return requests.get(self.base_url + '/candidates')
 
-
-    def get_all_positions(self):
+    def get_all_positions (self):
         return requests.get(self.base_url + '/positions')
-
 
     def perform_user_verification (self):
         return self.session.post(self.base_url + '/verify')
 
-
     def get_candidate_positions (self, user_id):
         return self.session.get(self.base_url + '/candidates/' + str(user_id) + '/positions')
+
+    def assign_positions_for_candidate(self, user_id, position_id):
+        return self.session.post(self.base_url + '/candidates/' + str(user_id) + '/positions' + str(position_id))
+
+
+# https://recruit-portnov.herokuapp.com/recruit/api/v1/candidates/2/positions   -position POST posit-9 usID-2
 
 # goal: token will be part of the session all the time we don't need to pass token to the func
 # all functions need to be in 1 container, that session can be shared,all function can use it.
--- test/my_test.py
+++ test/my_test.py
@@ -3,7 +3,6 @@ My Homework #2.
 """
 from faker import Faker
 import unittest
-
 from lib.authentication import Authenticate
 
 
@@ -39,45 +38,60 @@ class CareerPortalTests(unittest.TestCase):
         # login created candidate
         response = self.sess.authenticate(new_candidate_data['email'], new_candidate_data['password'])
         self.assertTrue(response.ok)
-
-        # Retrieve the list of all candidates and check that one of the entries is the candidate you posted
-        candidates = self.get_candidates()
-        # Example of classic loop
-        # candidate_ids = []
-        # for candidate in candidates:
-        #     candidate_ids.append(candidate['id'])
+        #
+        # # Retrieve the list of all candidates and check that one of the entries is the candidate you posted
+        # candidates = self.get_candidates()
+        # # Example of classic loop
+        # # candidate_ids = []
+        # # for candidate in candidates:
+        # #     candidate_ids.append(candidate['id'])
+        # # self.assertIn(new_candidate_id, candidate_ids)
+        # candidate_ids = [candidate['id'] for candidate in candidates]
         # self.assertIn(new_candidate_id, candidate_ids)
-        candidate_ids = [candidate['id'] for candidate in candidates]
-        self.assertIn(new_candidate_id, candidate_ids)
+        #
+        # # GET the list of existing candidates AGAIN...
+        # candidates = self.get_candidates()
+        # # Ensure the count is now greater than before
+        # self.assertEqual(len(candidates), candidates_count + 1)
+        #
+        # # Login as student@example.com and
+        # # DELETE the candidate you created using the ID you stored
+        # self.sess.authenticate("student@example.com", "welcome")
+        # response = self.sess.delete_candidate_by_id(new_candidate_id)
+        # self.assertTrue(response.ok)  # keep in mind that we should get 204 status here
+        # # Optionally we can check exact status with
+        # # self.assertEqual(response.status_code, 204)
+        #
+        # # Ensure the candidate was deleted!
+        # # 1) request candidates list again and ensure users count equals candidates_count - 1
+        # # 2) request candidate by id, ensure that API replies with 404 status
+        # response = self.sess.get_candidate_by_id(new_candidate_id)
+        # self.assertEqual(response.status_code, 400)
+        #
+        # # Retrieve the list of all candidates and check that the deleted candidate is no longer included in the list
+        # candidates = self.get_candidates()
+        # candidate_ids = [candidate['id'] for candidate in candidates]
+        # self.assertNotIn(new_candidate_id, candidate_ids)
+        #
+        # # создаем кандидату 2 позиции
+        # response = self.sess.assign_positions_for_candidate(new_candidate_id, '9')
+        # self.assertTrue(response.ok)
+        # position_for_new_candidate = response.json()
+        # self.assertEqual(position_for_new_candidate['id'], 9)
 
-        # GET the list of existing candidates AGAIN...
-        candidates = self.get_candidates()
-        # Ensure the count is now greater than before
-        self.assertEqual(len(candidates), candidates_count + 1)
+        # убедиться, что у кандидата 2 позиции
+        response = self.sess.get_candidate_positions(new_candidate_id)
+        self.assertTrue(response.ok)
 
-        # Login as student@example.com and
-        # DELETE the candidate you created using the ID you stored
-        self.sess.authenticate("student@example.com", "welcome")
-        response = self.sess.delete_candidate_by_id(new_candidate_id)
-        self.assertTrue(response.ok)  # keep in mind that we should get 204 status here
-        # Optionally we can check exact status with
-        # self.assertEqual(response.status_code, 204)
 
-        # Ensure the candidate was deleted!
-        # 1) request candidates list again and ensure users count equals candidates_count - 1
-        # 2) request candidate by id, ensure that API replies with 404 status
-        response = self.sess.get_candidate_by_id(new_candidate_id)
-        self.assertEqual(response.status_code, 400)
 
-        # Retrieve the list of all candidates and check that the deleted candidate is no longer included in the list
-        candidates = self.get_candidates()
-        candidate_ids = [candidate['id'] for candidate in candidates]
-        self.assertNotIn(new_candidate_id, candidate_ids)
 
-    def test_cannot_login(self):
-        response = self.sess.authenticate('foo', 'barr')
-        json_parsed = response.json()
-        self.assertEqual('Incorrect email: foo', json_parsed['errorMessage'])
+
+    #
+    # def test_cannot_login(self):
+    #     response = self.sess.authenticate('foo', 'barr')
+    #     json_parsed = response.json()
+    #     self.assertEqual('Incorrect email: foo', json_parsed['errorMessage'])
 
 
 if __name__ == '__main__':
